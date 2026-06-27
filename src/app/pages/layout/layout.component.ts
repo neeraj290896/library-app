807c838e7438b5cd8a57ee01a107955edf3ffcf8
neeraj290896 <neeraj.muduli29@gmail.com>
2026-06-27 17:31:14 +0530

    Auth Guard Configured

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -60,10 +60,7 @@ export class LayoutComponent {
     logout(): void {
         this.authService.logout();
 
-        localStorage.clear(); 
-        sessionStorage.clear();
-
-        this.router.navigate(['/login'], { replaceUrl: true });
+        this.router.navigate(['/login']);
     }
 
     toggleSidebar(): void {
